437d628ebe00225fc91ad6e6a57341733aa785c3
LiuLiu <1793515461@qq.com>
2019-10-09 17:36:56 +0800

    更改了图标及加入了扩张的css文件

--- src/main.js
+++ src/main.js
@@ -7,9 +7,13 @@ import app from './App.vue'
 
 //导入mui样式表
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
+
 
 // 按需导入mint-ui组件中的Header
 import { Header } from 'mint-ui';
+
+
 Vue.component(Header.name, Header);
 
 
